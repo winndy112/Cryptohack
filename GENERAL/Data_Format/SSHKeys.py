757edf9c1e46d08e9768bd0491617472c64a53bb
NGUYEN THI QUYNH ANH <127226698+ik-nown@users.noreply.github.com>
2023-04-23 17:18:24 +0700

    Update and rename test.py to SSHKeys.py

---
+++ GENERAL/Data_Format/SSHKeys.py
@@ -0,0 +1,7 @@
+from Crypto.PublicKey import RSA
+
+f = open(r'D:\Cryptohack\GENERAL\Data_Format\bruce_rsa_6e7ecd53b443a97013397b1a1ea30e14.pub', 'r')
+pubkey = RSA.import_key(f.read())
+
+print(pubkey.n)
+        print("{}: {}".format(info._oid._name, info._value))
