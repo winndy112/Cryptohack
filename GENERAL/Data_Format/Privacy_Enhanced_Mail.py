b638798242b329eebe3c23931398629ada112c19
NGUYEN THI QUYNH ANH <127226698+ik-nown@users.noreply.github.com>
2023-04-24 10:54:46 +0700

    Update Privacy_Enhanced_Mail.py

--- GENERAL/Data_Format/Privacy_Enhanced_Mail.py
+++ GENERAL/Data_Format/Privacy_Enhanced_Mail.py
@@ -5,4 +5,4 @@ from Crypto.PublicKey import RSA
 with open(r'D:\Cryptohack\GENERAL\Data_Format\privacy_enhanced_mail_1f696c053d76a78c2c531bb013a92d4a.pem', 'r') as file:
     key = file.read()
     rsa_key = RSA.importKey(key)
-    print(rsa_key.has_private)
+    print(rsa_key.d)
